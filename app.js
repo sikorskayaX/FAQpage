e0be44461b00f00e077bd1beb839d63d2f916ace
sikorskayaX <sikorskaya02@gmail.com>
2023-12-01 04:44:30 +0300

    Add FAQ page functionality and styling

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-﻿
+﻿document.addEventListener('DOMContentLoaded', (event) => {
+    const toggles = document.querySelectorAll('.question-btn');
+
+    toggles.forEach(toggle => {
+        toggle.addEventListener('click', () => {
+            toggle.parentNode.classList.toggle('active');
+        })
+    })
+});
